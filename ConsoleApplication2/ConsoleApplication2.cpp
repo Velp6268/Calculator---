ec6feedd206eb1919c3af9ad71fc2f8dc8dea6fb
bogor <bogor@DESKTOP-DC6RO5J>
2021-12-12 16:45:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int a = 0, b = 0;
+    char operation;
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Введите операцию: ";
+    cin >> operation;
+
+    switch (operation) {
+    case '+':
+        cout << "Ответ: " << a + b << endl;
+        break;
+    case '-':
+        cout << "Ответ: " << a - b << endl;
+        break;
+    case '*':
+        cout << "Ответ: " << a * b << endl;
+        break;
+    case '/':
+        cout << "Ответ: " << a / b << endl;
+        break;
+    default:
+        cout << "Ошибка" << endl;
+    }
+    return 0;
+}
